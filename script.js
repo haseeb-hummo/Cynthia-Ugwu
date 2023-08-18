7583949ba8efcedde89f5819cd46e9c8a1b70fc5
ksahh <haseebshaikh25ks@gmail.com>
2023-08-18 19:42:29 +0530

    Custom Cursor added.

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+const customCursor = document.querySelector("[data-custom-cursor]");
+
+function customCursorFun() {
+    window.addEventListener("mousemove" , function(e) {
+        const positionX = e.clientX - customCursor.offsetWidth / 2;
+        const positionY = e.clientY - customCursor.offsetHeight / 2;
+
+        customCursor.style.transform = `translate(${positionX}px , ${positionY}px)`;
+
+    })
+}
+
+customCursorFun()
