745c5772e0008f6b94d27f4d6eebf22029205aa7
ksahh <haseebshaikh25ks@gmail.com>
2023-08-26 13:31:32 +0530

    First page and cursor animation added.

--- script.js
+++ script.js
@@ -4,16 +4,87 @@ const scroll = new LocomotiveScroll({
 });
 
 
-const customCursor = document.querySelector("[data-custom-cursor]");
 
-function customCursorFun() {
-    window.addEventListener("mousemove" , function(e) {
-        const positionX = e.clientX - customCursor.offsetWidth / 2;
-        const positionY = e.clientY - customCursor.offsetHeight / 2;
+function firstPageAnimation() {
+    var t1 = gsap.timeline();
 
-        customCursor.style.transform = `translate(${positionX}px , ${positionY}px)`;
+    t1.from("nav", {
+        y: -15,
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeOut
+    });
+
+    t1.to(".heading-animation > .hidden", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1.5,
+        stagger: 0.2
+    });
+
+    t1.from(".bottomText2", {
+        y: 15,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1.5,
+        ease: Expo.easeInOut,
+    })
+}
+
+
+
+
+function cursorAnimation() {
+
+    let cursorScaleX = 1;
+    let cursorScaleY = 1;
+
+    let prevX = 0;
+    let prevY = 0;
+
+    window.addEventListener("mousemove", (e) => {
+
+        let diffX = e.clientX - prevX;
+        let diffY = e.clientY - prevY;
+
+        prevX = e.clientX;
+        prevY = e.clientY;
+
+        cursorScaleX = gsap.utils.clamp(0.8, 1.2, diffX);
+        cursorScaleY = gsap.utils.clamp(0.8, 1.2, diffY);
+
+        customCursorFun(e.clientX , e.clientY ,  cursorScaleX , cursorScaleY)
+        // console.log(cursorScaleX , cursorScaleY)
 
     })
 }
 
-customCursorFun()
+cursorAnimation();
+
+
+const customCursor = document.querySelector("[data-custom-cursor]");
+
+function customCursorFun(clientX , clientY , cursorScaleX, cursorScaleY) {
+    
+            const positionX = clientX - customCursor.offsetWidth / 2;
+            const positionY = clientY - customCursor.offsetHeight / 2;
+    
+            customCursor.style.transform = `translate(${positionX}px , ${positionY}px) scale(${cursorScaleX} , ${cursorScaleY})`;
+    
+    }
+    customCursorFun();
+    
+    
+
+
+
+firstPageAnimation();
+// function customCursorFun(){
+    
+//     window.addEventListener("mousemove" , (e) => {
+//         document.querySelector("[data-custom-cursor]").style.transform = `translate(${e.clientX}px - .offsetWidth / 2, ${e.clientY}px) - .offsetHeight / 2`
+//     })
+    
+// }
+// customCursorFun();
